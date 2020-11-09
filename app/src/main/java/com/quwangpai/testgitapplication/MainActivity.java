384af4282c119de42c4cab713d290e35caa7fa90
yyj <yiyajing0921@yeah.net>
2020-11-09 11:14:10 +0800

    2222

--- app/src/main/java/com/quwangpai/testgitapplication/MainActivity.java
+++ app/src/main/java/com/quwangpai/testgitapplication/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第一次提交
-        //修改TAG后提交
-        //测试分支内容
+        //首先提交tag内容
 
     }
 }
